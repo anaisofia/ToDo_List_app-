645b192ff3914b8dd6546772ce2cbaa564a76d6d
Anaisofia <anaisrivas.26@gmail.com>
2019-06-12 19:54:02 -0400

    creación de ruta, método y vista de Create, se redirige a index al almacenar

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,15 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.create
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
